b1208c296df6da8f01aa9df1cfdfe10552bff5e7
[Joshua Taiwo] <[joshuataiwo07@gmail.com]>
2023-04-24 23:18:05 +0100

    I added the play again functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,166 +1,18 @@
-inputs = {};
-name1 = name2 = name3 = name4 = "";
-function game(name1, name2, name3, name4, nplayer){
-    score1 = 0;
-    score2 = 0;
-    score3 = 0;
-    score4 = 0;
-    final = 0;
-    names = "";
-    $("#score1").text(score1);
-    $("#score2").text(score2);
-    $("#score3").text(score3);
-    $("#score4").text(score4);
-    $("#playerwr").fadeOut(1000, function(){
-        let players = [name1, name2, name3, name4];
-        $("#turn").text(players[0]);
-        namelist = listed.length;
-        m = Math.floor(Math.random() * namelist);
-        n = parseInt(nplayer);
-        names = listed[m].toUpperCase();
-        console.log(names);
-        var l = names.length;
-        let pname = $(".pname");
-        for(i = 0; i < l; i++){
-            $("#namew").append('<div class="divs"></div>')
-        };
-        for(i = 0; i < n; i++){
-            pname.eq(i).text(players[i]);
+function checkinput(input, string){
+    let length = string.length;
+    let present = 0;
+    let positions = [0];
+    for(i = 0; i < length; i++){
+        if(input === string[i]){
+            present++;
+            positions[0] = present;
+            positions.push(i);
         }
-        let player1 = $("#player1");
-        let player2 = $("#player2");
-        let player3 = $("#player3");
-        let player4 = $("#player4");
-        if(n === 1){
-            player1.show();
-        }else if(n === 2){
-            player1.show();
-            player2.show();
-        }else if(n === 3){
-            player1.show();
-            player2.show();
-            player3.show();
-        }else if(n === 4){
-            player1.show();
-            player2.show();
-            player3.show();
-            player4.show();
-        }
-        $(".special").fadeIn(3000, function(){
-            pturn = 0;
-            $("#check").click(function(e){
-                e.preventDefault();
-                let letter = $("#guess").val().toUpperCase();
-                if(letter != ""){
-                    let x = $(".divs");
-                    let q = 1;
-                    let score = 0;
-                    console.log(inputs[letter]);
-                    if(inputs[letter] != undefined){
-                        $("#exist").show();
-                        $("#right, #wrong, #empty").hide();
-                    }else{
-                        for(i = 0; i < x.length; i++){
-                            me = 0;
-                            if(letter === names[i]){
-                                q = 0;
-                                x.eq(i).text(letter.toUpperCase(), score++);
-                                $("#right, #continue").show();
-                                $("#form, #wrong, #empty, #exist").hide();
-                            }else{
-                                score += 0;
-                            }
-                            if(q === 1){
-                                me = 1;
-                                $("#form, #right, #exist, #empty").hide();
-                                $("#continue, #wrong").show();
-                            }
-                        }
-                        inputs[letter] = score;
-                        let scored = 0;
-                        if(me === 0){
-                            scored = (10 * score);
-                        }
-                        $("#score1").text(score1);
-                        $("#score2").text(score2);
-                        $("#score3").text(score3);
-                        $("#score4").text(score4);
-                        if(pturn === 0){
-                            score1 += scored;
-                            $("#score1").text(score1);
-                        }else if(pturn === 1){
-                            score2 += scored;
-                            $("#score2").text(score2);
-                        }else if(pturn === 2){
-                            score3 += scored;
-                            $("#score3").text(score3);
-                        }else if(pturn === 3){
-                            score4 += scored;
-                            $("#score4").text(score4);
-                        }
-                        final = score1 + score2 + score3 + score4;
-                        if(final == (l * 10)){
-                            winner = Math.max(score1, score2, score3, score4);
-                            if(winner === score1){
-                                $("#winner").text(name1 + " is the winner!!!");
-                            }else if(winner === score2){
-                                $("#winner").text(name2 + " is the winner!!!");
-                            }else if(winner === score3){
-                                $("#winner").text(name3 + " is the winner!!!");
-                            }else if(winner === score4){
-                                $("#winner").text(name4 + " is the winner!!!");
-                            }
-                            $("#winner").fadeIn(4000, function(){
-                                $(document).click(function(){
-                                    stopConfetti();
-                                    $("#winner").fadeOut(2000, function(){
-                                        $(".pagew").css("zIndex", 1);
-                                        $("#gover").show(function(){
-                                            $("#gover").animate({opacity: '1'}, 5000);
-                                            $("#again").show(function(){
-                                                $("#again").animate({opacity: '1'}, 5000);
-                                            });
-                                        }) 
-                                    })
-                                })
-                            })
-                            startConfetti();
-                            $(".pagew").css("zIndex", -1);
-                            $(".special").fadeOut(2000); 
-                        }
-                    }
-                }else{
-                    $("#empty").show();
-                    $("#right, #wrong, #exist").hide();
-                }
-            });
-            n = parseInt(nplayer);
-            $("#continue").click(function(){
-                $("#right, #wrong, #continue").hide();
-                $("#form").show();
-                $("#guess").val('');
-                pturn++;
-                if(pturn === n){
-                    pturn = 0;
-                }
-                $("#turn").text(players[pturn]);
-            });
-            $("#pass").click(function(){
-                $("#empty, #right, #wrong, #exist").hide();
-                $("#guess").val('');
-                n = parseInt(nplayer);
-                pturn++;
-                if(pturn === n){
-                    pturn = 0;
-                }   
-                $("#turn").text(players[pturn]);
-            })
-        }
-        );
-    });    
-}   
+    }
+    return positions;
+}
 
-$(document).ready(function(){
+$(document).on("ready", function(){
     $("#tjg").fadeIn(2000, function(){
         $("#tjg").fadeOut(1000);
         $("#present").fadeIn(2000, function(){
@@ -179,7 +31,7 @@ $(document).ready(function(){
             });
         });
     });
-    $("input[name='player']").click(function(){
+    $("input[name='player']").on("click", function(){
         nplayer = $(this).val();
         if(nplayer === "1"){
             $("#playerw").fadeOut(1000, function(){
@@ -203,10 +55,7 @@ $(document).ready(function(){
             }); 
         }
     });
-    $("#submit").click(function(event){
-        event.preventDefault();
-    })
-    $("#submit").click(function(){
+    $("#submit").on("click", function(){
         name1 = $("#name1").val();
         name2 = $("#name2").val();
         name3 = $("#name3").val();
@@ -215,19 +64,161 @@ $(document).ready(function(){
             $("#your").show();
         }else{
             $("#submit").prop("disabled", true);
-            game(name1, name2, name3, name4, nplayer);   
+
+            function game(){
+
+                let inputs = {};
+                //Get Random Name
+                let namelist = listed.length;
+                let m = Math.floor(Math.random() * namelist);
+                let names = listed[m].toUpperCase();
+                let l = names.length;
+                let final = l * 10;
+                let n = parseInt(nplayer);
+                pturn = 0; // Current Player Turn
+
+                // Create Dashes
+                for(i = 0; i < l; i++){
+                    $("#namew").append('<div class="divs"></div>')
+                };
+
+                $("#playerwr").fadeOut(1000, function(){
+
+                    //Create Players Name and Scores
+                    let players = [name1, name2, name3, name4];
+                    $("#turn").text(players[0]);
+                    score1 = score2 = score3 = score4 = total = 0;
+                    if(n === 1){
+                        $("#player1").show();
+                    }else if(n === 2){
+                        $("#player1, #player2").show();
+                    }else if(n === 3){
+                        $("#player1, #player2, #player3").show();
+                    }else if(n === 4){
+                        $("#player1, #player2, #player3, #player4").show();
+                    }
+                    for(i = 0; i < n; i++){
+                        $(".pname").eq(i).text(players[i]);
+                    }
+
+                    $(".special").fadeIn(3000, function(){
+                        $("#check").on("click", function(){
+                            let letterinput = $("#guess").val().toUpperCase();
+                            $("#guess").attr("readonly", true);
+
+                            //If input is empty
+                            if(letterinput == ''){
+                                $("#empty").show();
+                                $("#right, #wrong, #exist").hide();
+                                $("#guess").attr("readonly", false);
+                            }else{
+                                //Check if input has been picked before
+                                if(inputs[letterinput] == undefined){
+                                    let test = checkinput(letterinput, names);
+                                    inputs[letterinput] = test[0];
+                                    if(test[0] == 0){
+                                        $("#wrong, #continue").show();
+                                        $("#form, #right, #empty, #exist").hide();
+                                    }else{
+                                        //Insert inputs in divs
+                                        for(p = 1; p < test.length; p++){
+                                            $(".divs").eq(test[p]).text(letterinput);
+                                        }
+                                        $("#right, #continue").show();
+                                        $("#form, #wrong, #empty, #exist").hide();
+                                        
+                                        //Display and Calculation of Scores
+                                        score = 10 * test[0];
+                                        if(pturn == 0){
+                                            score1 += score;
+                                            $("#score1").text(score1);
+                                        }else if(pturn == 1){
+                                            score2 += score;
+                                            $("#score2").text(score2);
+                                        }else if(pturn == 2){
+                                            score3 += score;
+                                            $("#score3").text(score3);
+                                        }else if(pturn == 3){
+                                            score4 += score;
+                                            $("#score4").text(score4);
+                                        }
+
+                                        total = score1 + score2 + score3 + score4;
+                                        if(total === final){
+                                            winner = Math.max(score1, score2, score3, score4);
+                                            if(winner === score1){
+                                                $("#winner").text(name1 + " is the winner!!!");
+                                            }else if(winner === score2){
+                                                $("#winner").text(name2 + " is the winner!!!");
+                                            }else if(winner === score3){
+                                                $("#winner").text(name3 + " is the winner!!!");
+                                            }else if(winner === score4){
+                                                $("#winner").text(name4 + " is the winner!!!");
+                                            }
+                                            $("#winner").fadeIn(4000, function(){
+                                                setTimeout(function(){
+                                                    stopConfetti();
+                                                    $("#winner").fadeOut(2000, function(){
+                                                        $("#gover").show(function(){
+                                                            $(".pagew").css("zIndex", 1);
+                                                            $("#gover").animate({opacity: '1'}, 5000);
+                                                            $("#over>center>button").show(function(){
+                                                                $("#over>center>button").animate({opacity: '1'}, 5000);
+                                                                $("#again").prop("disabled", false);
+                                                            });
+                                                        }) 
+                                                    })
+                                                }, 3000)
+                                            })
+                                            startConfetti();
+                                            $(".pagew").css("zIndex", -1);
+                                            $(".special").fadeOut(2000);
+                                        }
+                                    }
+                                }else{
+                                    $("#empty, #right, #wrong").hide();
+                                    $("#exist").show();
+                                    $("#guess").attr("readonly", false);
+                                }
+                            }
+                        });
+
+                        $("#continue").on("click", function(){
+                            $("#right, #wrong, #continue").hide();
+                            $("#form").show();
+                            $("#guess").attr("readonly", false);
+                            $("#guess").val('');
+                            pturn++;
+                            if(pturn == n){
+                                pturn = 0;
+                            }
+                            $("#turn").text(players[pturn]);
+                        });
+
+                        $("#pass").on("click", function(){
+                            $("#empty, #right, #wrong, #exist").hide();
+                            $("#guess").val('');
+                            pturn++;
+                            if(pturn == n){
+                                pturn = 0;
+                            }
+                            $("#turn").text(players[pturn]);
+                        });
+
+                    });
+                });
+            }
+            game();
+            $("#again").on("click", function(){
+                $("#score1, #score2, #score3, #score4").text(0);
+                $("#over>center>button, #gover").css("opacity", 0);
+                $("#guess").attr("readonly", false);
+                $("#over>center>button, #gover, #empty, #right, #wrong, #exist, #continue").hide();
+                $("#form").show();
+                $("#guess").val('');
+                $(".divs").remove();
+                game();
+            });
         }
     });
 })
-
-$("#again").click(function(){
-    $("#gover, #again, .none").hide();
-    $("#gover, #again").css("opacity", 0);
-    $("#form").show();
-    $(".divs").remove();
-    $("#guess").val("");
-    for(const key in inputs){
-        delete inputs[key];
-    }
-    game(name1, name2, name3, name4, nplayer);
-}); 
